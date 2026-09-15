1892fe11e594faae4fce20b89e3738b4db9a8d99
Victor <vmmzdev@gmail.com>
2026-09-15 09:33:34 +0200

    refactor:cambio de path en ConfirmDialog.jsx pasa a ser un componente

--- src/pages/AdminReservas.jsx
+++ src/pages/AdminReservas.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ReservaCard from "../components/ReservaCard";
 import EditReservaModal from "../utils/EditReservaModal";
-import ConfirmDialog from "../utils/ConfirmDialog";
+import ConfirmDialog from "../components/ConfirmDialog";
 import InfoClienteModal from "../utils/InfoClienteModal";
 import {
   getReservas,
